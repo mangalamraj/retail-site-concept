f61c35580f45f274c985fb6b4ff72c88acbf3540
Mangalam Raj <mango.26june@gmail.com>
2026-02-13 13:26:50 +0530

    progress

--- components/trustedBy/trustedBy.tsx
+++ components/trustedBy/trustedBy.tsx
@@ -31,8 +31,8 @@ const TrustedBy = () => {
             </div>
           ))}
         </Marquee>
-        <div className="dark:from-background from-zinc-50 pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
-        <div className="dark:from-background from-zinc-50 pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
+        <div className="dark:from-black from-zinc-50 pointer-events-none absolute inset-y-1 pl-2 left-0 w-1/4 bg-gradient-to-r"></div>
+        <div className="dark:from-black from-zinc-50 pointer-events-none absolute inset-y-1 pr-2 right-0 w-1/4 bg-gradient-to-l"></div>
       </div>
     </div>
   );
